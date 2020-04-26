d08e0014869e780f7cb871c67e7578e312844017
zaozaofeng <188369300@qq.com>
2020-04-26 19:11:47 +0800

    压缩文件倒是可以生成了，但是压缩率感人，平均码长趋近于88

--- Huffman_encoder.v
+++ Huffman_encoder.v
@@ -9,6 +9,7 @@
 //压缩后文件头格式：符号8bit+码长3bit+码字1-8bit
 //`define SEND_SYMBOLS 3'b101//发送符号
 `define SEND_CODE 3'b110	//发送编码6
+`define GEN_FILE 3'b101     //生成压缩文件5
 `define IDLE 3'b111 //休息状态
 //`define SEND_LENGTH 3'b111	//发送长度
 
@@ -19,6 +20,7 @@ module Huffman_encoder(
 			input wire data_enable,	//置高才可接收数据
 
             input wire rst,//重置信号
+           // input wire fp_r,
             
             //本模块的输出数据
 			output reg [bit_width:0]data_out_symbol,    //输出-符号
@@ -31,12 +33,13 @@ module Huffman_encoder(
 						
 parameter bit_width = 7;//每个符号的位宽
 parameter max_symbol = 255;//符号上限数
-parameter length_of_Data = 100;//数据长度
+parameter length_of_Data = 1024;//数据长度
 
 reg [2:0]state = `INIT;	//当前状态值
 
 reg [bit_width:0]symbol_list[max_symbol:0];//符号表
 integer symbol_count = 0;//符号数
+integer data_length = 0;
 reg [9:0]freq_list[max_symbol:0];//频次表
 reg [255:0]symbol_list_index = 'b0;	//符号表指针
 
@@ -65,9 +68,56 @@ integer i= 32'h0;
 integer j= 32'h0;
 integer k= 32'h0;//循环变量
 
+//写压缩文件相关
+integer fp_r; //写文件句柄
+
+reg [11:0]filehead_length = 'b0; //文件头长度
+reg [17:0]filehead_symfreq = 'b0; //每个符号和频率的暂存区
+reg [2*bit_width+2:0]temp_code; //码字暂存（前面带XXX，需取出正确码字）
+reg [4*bit_width+4 : 0]buffer_fwrite = 'b0;//写文件时的二进制流缓冲区
+integer buffer_fwrite_index = 4*bit_width+4;//当前缓冲区位置索引
+
+
+
 reg flag_exist = 0;//当前符号是否存在于表中的标志位 1=存在 0=不存在
 
+//二进制转base64函数
+function [7:0]bin2base64;//返回8位，对应ASCII码
+    input [5:0]input_bin; //输入6位，对应BASE64索引
+    reg [7:0]temp_out;
+   
+    begin
+    /* case (input_bin)
+        0:begin
+            temp_out = 41;
+        end
 
+    endcase*/
+
+    if(input_bin <= 25 && input_bin >=0 )begin
+        //A-Z
+        temp_out = input_bin + 65;
+    end 
+    else if(input_bin <= 51 && input_bin >=26)begin
+        //a-z
+        temp_out = input_bin + 71;
+    end
+    else if(input_bin <= 61 && input_bin >=52)begin
+        //0-9
+        temp_out = input_bin -  4;
+    end
+    else if(input_bin == 62)begin
+        // '+'
+        temp_out = 43;
+    end
+    else if(input_bin == 63)begin
+        // '/'
+        temp_out = 47;
+    end
+    bin2base64 = temp_out;
+
+    end
+endfunction
 
 always @(posedge clock) begin
 
@@ -103,6 +153,7 @@ always @(posedge clock) begin
     //频次统计
 	if(data_enable) begin   //仅当此位为高时进行数据输入&统计
 		data[i] = data_in;
+        data_length = data_length +1 ;
 		i=i+1'b1;
 		
 			for(j=0;j<=max_symbol; j=j+1) begin
@@ -135,7 +186,7 @@ always @(posedge clock) begin
         //debug用：
         $display("symbol_list_index:",symbol_list_index);
 		for(i=0;i<symbol_count;i=i+1)begin
-        $display("i=",i,"  symbol_list:",symbol_list[i],"  freq_list:",freq_list[i]); end
+        $display("i=",i,"  symbol_list:%c",symbol_list[i],"  freq_list:",freq_list[i]); end
     end
 	end
 	
@@ -168,7 +219,7 @@ always @(posedge clock) begin
     end
     //debug用：
     for(i=1;i<symbol_count+1;i=i+1)begin
-    $display("i=",i,"  huff-symbol :",huffmantree_node_symbol[i],"   huff-weight:",huffmantree_node_weight[i]);end
+    $display("i=",i,"  huff-symbol :%c",huffmantree_node_symbol[i],"   huff-weight:",huffmantree_node_weight[i]);end
     state = `BUILD_TREE;    //进入构建阶段
     out_state = state;
     huffmantree_node_weight[0] = 1023;  //
@@ -229,7 +280,7 @@ always @(posedge clock) begin
     if(min1 == 0 && min2 == 0)begin //当树构建完成
        // debug用：
          for(i=0;i<=symbol_list_index;i=i+1)
-         $display("i=",i,"  sym=",huffmantree_node_symbol[i],"  lchild:",
+         $display("i=",i,"  sym=%c",huffmantree_node_symbol[i],"  lchild:",
           huffmantree_node_lchild[i],"  rchild:",  huffmantree_node_rchild[i],
           "  weight:", huffmantree_node_weight[i]," parent:", huffmantree_node_parent[i]," tag:", huffmantree_node_tag[i]);
        //进入下一环节
@@ -281,7 +332,7 @@ always @(posedge clock) begin
         //debug
         if(i==0)begin
         for(i=0;i<symbol_count;i=i+1)begin
-                 $display("i=%d",i," symbol:", symbol_list[i],"  length:%d",code_length[i],
+                 $display("i=%d",i," symbol:%c", symbol_list[i],"  length:%d",code_length[i],
                     "  code:%b",code_list[i]);
             end
         i=0;
@@ -298,11 +349,71 @@ always @(posedge clock) begin
         end
         else begin
             data_out_state = 0;
-            state = `IDLE;
+            state = `GEN_FILE;
         end
 
 	
 	end
+    `GEN_FILE:begin
+        fp_r=$fopen("data_out_compressed_2.zzf","w");//以写的方式打开文件
+
+        //$display("symbol count = ", symbol_count);
+        //$display("data length = ",data_length);
+        //$display("0-> base64=",bin2base64(6'b000000));
+
+        //写入文件头信息
+        //用12bit表示文件头长度，转换为2个base64字符
+        filehead_length = symbol_count; //文件头长度，即符号重量的数量
+        
+
+        //$display("%d%d",bin2base64(filehead_length[11:6]),bin2base64(filehead_length[5:0]));
+        $fwrite(fp_r,"%c%c",bin2base64(filehead_length[11:6]),bin2base64(filehead_length[5:0]));
+        
+        //写入符号和频率信息，每个符号和频率占用18bit,其中8bit表示符号，10bit表示对应的频率（权重）
+        //最终转换成3个BASE64字符
+        for(i=0 ;  i < symbol_count; i=i+1)begin
+            filehead_symfreq[17:10] = symbol_list[i];
+            filehead_symfreq[9:0] = freq_list[i];
+            $display("sym=%c",symbol_list[i],"  freq=%d",freq_list[i],"    filehead_symfreq= %b",filehead_symfreq);
+            $fwrite(fp_r,"%c%c%c",bin2base64(filehead_symfreq[17:12]),bin2base64(filehead_symfreq[11:6]),bin2base64(filehead_symfreq[5:0]));
+        end
+
+
+        //写入正文信息
+        for(k=0 ;   k < data_length;  k = k+1)begin
+            for(j = 0; j < symbol_count; j = j + 1)begin
+                if(data[k] == symbol_list[j])begin //匹配到字符
+                $display("data = %c",symbol_list[j]);
+                    if(buffer_fwrite_index > 0)begin //确保写入缓冲区时还有空间
+                        temp_code = code_list[j];
+                        //$display("temp_code = %b",temp_code);
+
+                        //将哈夫曼码字逐位（划重点)写入缓冲区
+                        for(i=code_length[j]-1 ; i >= 0; i=i-1)begin
+                           buffer_fwrite[buffer_fwrite_index] = temp_code[i];
+                           buffer_fwrite_index = buffer_fwrite_index - 1;
+                         end
+                    end
+                end
+            end
+
+            $display("buffer_fwrite= %b",buffer_fwrite);
+
+            while(buffer_fwrite_index <= (4*bit_width+4 - 6))begin//只要满6位就写一个base64字符进文件
+                
+                $fwrite(fp_r,"%c",bin2base64(buffer_fwrite[4*bit_width+4:4*bit_width-1]));
+                buffer_fwrite = buffer_fwrite << 6; //每写入一个base64字符，左移6位缓冲区
+                $display("buffer_fwrite after << 6 : %b",buffer_fwrite);
+                buffer_fwrite_index = buffer_fwrite_index + 6;
+            end
+            
+            
+        end
+
+        $display("--------------------close file-----------------");
+        $fclose(fp_r); //关闭文件
+        state = `IDLE;
+    end
 	
     `IDLE:begin
 
--- huffman_tb.v
+++ huffman_tb.v
@@ -5,8 +5,12 @@ module Huffman_tb;
 
   // Inputs
 	reg clock;
-	reg [2:0] data_in;
+	reg [7:0] data_in;
 	reg data_enable;
+
+	reg [8:0]data[99:0];
+	reg [7:0]reg1,reg2,reg3 = 8'b0;
+	integer count = 0;
 
 	// Outputs
     wire [7:0]data_out_symbol;    //输出-符号
@@ -18,9 +22,12 @@ module Huffman_tb;
    
 	integer i,handle;
 	integer seed = 1;
+
+	integer fp_r;
+	integer fp_w;
 	
 	// Instantiate the Unit Under Test (UUT)
-	Huffman_encoder #(2,8,20) uut (
+	Huffman_encoder #(7,255,1024) uut (
 		.clock(clock), 
 		.data_in(data_in), 
 		.data_enable(data_enable), 
@@ -29,6 +36,7 @@ module Huffman_tb;
 		.data_out_code(data_out_code),
         .data_out_state(data_out_state),
 		.out_state(out_state)
+		//.fp_r(fp_r)
 	);
 
 
@@ -44,21 +52,63 @@ module Huffman_tb;
 		data_in = 0;
 		data_enable = 0;
 		
-		handle = $fopen ("Output.txt");
-		handle = handle | 1;
+		
+		//handle = $fopen ("Output.txt");
+		//handle = handle | 1;
+		//$readmemh("TEST.txt",data);
+	//	for(i=0;i<18;i=i+1)begin
+		//	$display("i=%d",i,"		data=%b",data[i]);
+	//	end
+		fp_r=$fopen("TEST4.txt","r");//以读的方式打开文件
+		//fp_w=$fopen("data_out2.dat","w");//以写的方式打开文件
+
+		//$display("fp_r=",fp_r,"fp_w=",fp_w);
+		//	while(! $feof(fp_r))begin
+					//$fscanf(fp_r,"%h" ,reg1) ;//每次读一行
+					//$display("::::%b",reg1) ;//打印输出
+					//$fwrite(fp_w,"%h",reg1,) ;//写入文件
+		//			reg1 = $fgetc(fp_r);
+					//$display("reg1=%b",reg1);
+		//			if(reg1 != 8'b11111111)begin
+		//			$display("char = %c",reg1,"	b=%b",reg1);
+		//			$fwrite(fp_w,"%c",reg1);//ascii
+		//			end
+					
+					//$fwrite(fp_w,"%b",reg1);
+
+
+		//	end
+			//$fclose(fp_r);//关闭已打开的文件
+			//$fclose(fp_w);
+
 		
 		$fmonitor(1,"clock:",clock,"  data_enable:", data_enable,"  data_in:%b",data_in,"  data_out_state:",data_out_state,"  data_out_code: %b",data_out_code,"  data_out_symbol:%b",data_out_symbol);
 		
-		for(i=0;i<22;i=i+1) begin 
+		/*for(i=0;i<22;i=i+1) begin 
 				#20 data_enable=1;
-				data_in = $random(seed);
+				//data_in = $random(seed);
+				reg1 = $fgetc(fp_r);
+				$display("reg1=%b",reg1);
+				if(reg1 != 8'b11111111)begin
+					data_in = reg1;
+				end
+		end
+		*/
+		while(! $feof(fp_r))begin
+			#20 data_enable=1;
+			reg1 = $fgetc(fp_r);
+			if(reg1 != 8'b11111111)begin
+				$display("char = %c",reg1,"	b=%b",reg1,"	ascii=%d",reg1);
+
+				data_in = reg1;
+			end
 		end
 		
 		#10
 		data_in = 'bz;
 		data_enable = 0;
 		
-		#2550;
+		#5000;
 		$finish;
 		$fclose(handle);
 
